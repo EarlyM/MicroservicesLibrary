b3f7716c9cb1778b1536b31deb29142836762d12
Aleksey <tacuma@lan.ua>
2017-12-14 17:33:57 +0200

    add

--- message-service/src/main/java/ua/microserviceslibrary/messageservice/repository/MessageRepository.java
+++ message-service/src/main/java/ua/microserviceslibrary/messageservice/repository/MessageRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 import ua.microserviceslibrary.messageservice.model.Message;
 
 import java.util.List;
-import java.util.Set;
 
 @Repository
 public interface MessageRepository extends CrudRepository<Message, Long>{
